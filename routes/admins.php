5ec74ae729efa79faf094ae23be7a6e82cd34764
Nishad Hossain <freelancernishad123@gmail.com>
2025-06-07 16:13:57 +0600

    update

--- routes/admins.php
+++ routes/admins.php
@@ -2,14 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AuthenticateAdmin;
-use App\Http\Controllers\Admin\TouristPlaceController;
-use App\Http\Controllers\Admin\TouristPlaceCategoryController;
+
 
 
 Route::middleware(AuthenticateAdmin::class)->group(function () {
     Route::prefix('admin')->group(function () {
-        Route::apiResource('tourist-place-categories', TouristPlaceCategoryController::class);
-        Route::apiResource('tourist-places', TouristPlaceController::class);
+
     });
 });
 
